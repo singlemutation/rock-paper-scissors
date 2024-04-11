15fa5511ab0e99eb3df06624585a5caa3fa90baf
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-04-11 00:03:28 -0400

    Added score keeping
    Need to figure out why it's incrementing twice each win

--- scripts.js
+++ scripts.js
@@ -15,27 +15,35 @@ function random(number) {
     return Math.floor(Math.random() * number);
     
 }
+let compScore = 0;
+let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if ( playerSelection === "rock" && computerSelection === "paper" ) {
+        compScore++
         return "You Lose! Paper beats Rock"
     }
     if ( ( playerSelection === "rock" ) && ( computerSelection === "scissors" ) ) {
+        playerScore++;
         return "You Win! Rock beats Scissors"
     }
     if ( playerSelection === computerSelection ) {
         return "Tie!"
     }
     if ( ( playerSelection === "paper" ) && ( computerSelection === "scissors" ) ) {
+        compScore++;
         return "You Lose! Scissors beats Paper"
     }
     if ( ( playerSelection === "paper" ) && ( computerSelection === "rock" ) ) {
+        playerScore++;
         return "You Win! Paper beats Rock"
     }
     if ( ( playerSelection === "scissors" ) && ( computerSelection === "paper" ) ) {
+        playerScore++;
         return "You Win! Scissors beats Paper"
     }
     if ( ( playerSelection === "scissors" ) && ( computerSelection === "rock" ) ) {
+        compScore++;
         return "You Lose! Rock beats Scissors"
     }
 
@@ -48,12 +56,26 @@ function playGame() {
     console.log("You picked: " + playerSelection);
     console.log("CPU picked: " + computerSelection)
     console.log(playRound(playerSelection, computerSelection));
+    console.log("You: " + playerScore + " CPU: " + compScore)   //it's adding 2 points for some reason
 }
 
 for (let i = 0; i < 5; i++) {
     playGame();
 }
-// const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-// const computerSelection = getComputerChoice();
+
+// function winner(compScore, playerScore) {
+//     playerScore = 0;
+//     compScore = 0;
+//     if compScore > playerScore {
+//         console.log("You lost the game!")
+//     }
+//     if compScore < playerScore {
+//         console.log("You won the game!")
+//     }
+//     else {
+//         console.log("You tied the computer!")
+//     }
+// }
+
 
 
