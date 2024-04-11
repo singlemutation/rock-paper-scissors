b4e9fb69ad2fd5936687ffa2a80a771bf8816dee
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-04-10 20:44:26 -0400

    Add playRound function to play a round of RPS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 function getComputerChoice() {
     let computerChoiceNumber = random(3);
-    let computerChoice = null;
+    let computerSelection;
     if ( computerChoiceNumber === 0 ) {
-            return computerChoice = "Rock"
+            return computerChoice = "rock"
     }
     if ( computerChoiceNumber === 1 ) {
-            return computerChoice = "Paper"
+            return computerChoice = "paper"
     }
     if ( computerChoiceNumber === 2 ) {
-            return computerChoice = "Scissors"
+            return computerChoice = "scissors"
     }
 }
 
@@ -17,4 +17,40 @@ function random(number) {
     
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if ( playerSelection === "rock" && computerSelection === "paper" ) {
+        return "You Lose! Paper beats Rock"
+    }
+    if ( ( playerSelection === "rock" ) && ( computerSelection === "scissors" ) ) {
+        return "You Win! Rock beats Scissors"
+    }
+    if ( ( playerSelection === "rock" ) && ( computerSelection === "rock" ) ) {
+        return "Tie!"
+    }
+    if ( ( playerSelection === "paper" ) && ( computerSelection === "paper" ) ) {
+        return "Tie!"
+    }
+    if ( ( playerSelection === "paper" ) && ( computerSelection === "scissors" ) ) {
+        return "You Lose! Scissors beats Paper"
+    }
+    if ( ( playerSelection === "paper" ) && ( computerSelection === "rock" ) ) {
+        return "You Win! Paper beats Rock"
+    }
+    if ( ( playerSelection === "scissors" ) && ( computerSelection === "paper" ) ) {
+        return "You Win! Scissors beats Paper"
+    }
+    if ( ( playerSelection === "scissors" ) && ( computerSelection === "rock" ) ) {
+        return "You Lose! Rock beats Scissors"
+    }
+    if ( ( playerSelection === "scissors" ) && ( computerSelection === "scissors" ) ) {
+        return "Tie!"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
+console.log("You picked: " + playerSelection);
+console.log("CPU picked: " + computerSelection)
+console.log(playRound(playerSelection, computerSelection));
